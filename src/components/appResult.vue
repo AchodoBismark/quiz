bb2bc6ad2b688cd61a1ad30851100067cc49843f
Bismark Achodo <achodobismark@gmail.com>
2022-03-12 19:15:27 +0100

    added quiz app

---
+++ src/components/appResult.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="result">
+    <div class="title">{{ results[resultIndex].title }}</div>
+    <div class="desc">
+      {{ results[resultIndex].desc }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["results", "totalCorrect"],
+  computed: {
+    resultIndex() {
+      let index = 0;
+      this.results.forEach((elem, i) => {
+        if (elem.min <= this.totalCorrect && elem.max >= this.totalCorrect) {
+          index = i;
+        }
+      });
+      return index;
+    },
+  },
+};
+</script>
